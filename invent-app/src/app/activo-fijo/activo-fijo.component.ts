ec37e4c0d11097afb74555c5c7ed92e64aff6b08
andrenimacion <andrenimacion@gmail.com>
2020-08-19 14:50:15 -0500

    Se esta terminando los inputs y comunicaciones

--- invent-app/src/app/activo-fijo/activo-fijo.component.ts
+++ invent-app/src/app/activo-fijo/activo-fijo.component.ts
@@ -14,6 +14,27 @@ export class ActivoFijoComponent implements OnInit {
   private _FFin: any;
   private _FCrea: any;
   private _FCust: any;
+  private _FeInde: any;
+  private _FCompra: any;
+  private _Imagen: any;
+  private _Placa: any;
+  private _CLase: any;
+  private _NProd: any;
+  private _Custodio: any;
+  private _Depart: any;
+  private _Serie: any;
+  private _Valor: any;
+  private _Activo: any;
+  private _Referido: any;
+  private _USC: any;
+  private _USM: any;
+  private _USF: any;
+  private _GRP: any;
+  private _MRC: any;
+  private _PROV: any;
+  private _MODL: any;
+  private _VUTL: any;
+  private _VRES: any;
 
   @Input()
   set Factual(value: any){
@@ -22,7 +43,6 @@ export class ActivoFijoComponent implements OnInit {
   get Factual():any {
     return this._Factual;
   }
-
   
   @Input()
   set FMod(value: any){
@@ -54,6 +74,174 @@ export class ActivoFijoComponent implements OnInit {
   }
   get FCust():any {
     return this._FCust;
+  }
+  
+  @Input()
+  set FeInde(value: any){
+    this._FeInde = value;
+  }
+  get FeInde():any {
+    return this._FeInde;
+  }
+  
+  @Input()
+  set FCompra(value: any){
+    this._FCompra = value;
+  }
+  get FCompra():any {
+    return this._FCompra;
+  }
+
+  @Input()
+  set Imagen(value: any){
+    this._Imagen = value;
+  }
+  get Imagen():any {
+    return this._Imagen;
+  }
+
+  @Input()
+  set Placa(value: any){
+    this._Placa = value;
+  }
+  get Placa():any {
+    return this._Placa;
+  }
+  
+  @Input()
+  set CLase(value: any){
+    this._CLase = value;
+  }
+  get CLase():any {
+    return this._CLase;
+  }
+
+  @Input()
+  set NProd(value: any){
+    this._NProd = value;
+  }
+  get NProd():any {
+    return this._NProd;
+  }
+
+  @Input()
+  set Custodio(value: any){
+    this._Custodio = value;
+  }
+  get Custodio():any {
+    return this._Custodio;
+  }
+
+  @Input()
+  set Depart(value: any){
+    this._Depart = value;
+  }
+  get Depart():any {
+    return this._Depart;
+  }
+
+  @Input()
+  set Serie(value: any){
+    this._Serie = value;
+  }
+  get Serie():any {
+    return this._Serie;
+  }
+
+  @Input()
+  set Valor(value: any){
+    this._Valor = value;
+  }
+  get Valor():any {
+    return this._Valor;
+  }
+
+  @Input()
+  set Activo(value: any){
+    this._Activo = value;
+  }
+  get Activo():any {
+    return this._Activo;
+  }
+
+  @Input()
+  set Referido(value: any){
+    this._Referido = value;
+  }
+  get Referido():any {
+    return this._Referido;
+  }
+
+  @Input()
+  set USC(value: any){
+    this._USC = value;
+  }
+  get USC():any {
+    return this._USC;
+  }
+
+  @Input()
+  set USM(value: any){
+    this._USM = value;
+  }
+  get USM():any {
+    return this._USM;
+  }
+
+  @Input()
+  set USF(value: any){
+    this._USF = value;
+  }
+  get USF():any {
+    return this._USF;
+  }
+
+  @Input()
+  set GRP(value: any){
+    this._GRP = value;
+  }
+  get GRP():any {
+    return this._GRP;
+  }
+
+  @Input()
+  set MRC(value: any){
+    this._MRC = value;
+  }
+  get MRC():any {
+    return this._MRC;
+  }
+
+  @Input()
+  set PROV(value: any){
+    this._PROV = value;
+  }
+  get PROV():any {
+    return this._PROV;
+  }
+
+  @Input()
+  set MODL(value: any){
+    this._MODL = value;
+  }
+  get MODL():any {
+    return this._MODL;
+  }
+
+  @Input()
+  set VUTL(value: any){
+    this._VUTL = value;
+  }
+  get VUTL():any {
+    return this._VUTL;
+  }
+
+  @Input()
+  set VRES(value: any){
+    this._VRES = value;
+  }
+  get VRES():any {
+    return this._VRES;
   }
 
   ngOnInit() {
--- invent-app/src/app/configurations/configurations.component.ts
+++ invent-app/src/app/configurations/configurations.component.ts
@@ -20,46 +20,68 @@ export class ConfigurationsComponent implements OnInit {
   public fecFin: any;
   public feCreac: any;
   public feCusto: any;
+  public feInde: any;
+  public fCompra: any;
+  public Imge: any;
+  public placa: any;
+  public Clas: any;
+  public nProd: any;
+  public nCustodios: any;
+  public ndepar: any;
+  public nSerie: any;
+  public nValor: any;
+  public nActivo: any;
+  public nUsuc: any;
+  public nUsm: any;
+  public UsFin: any;
+  public nGroup: any;
+  public mArca: any;
+  public ProvVar: any;
+  public ModVar: any;
+  public VitVar: any;
+  public ValrVar: any;
   // variables para los Inputs comunicacion de compronentes FIN
   
   // ngModel inicio
-  public _placa;
-  public _clase;
-  public _nombre;
-  public _custodio;
-  public _dpto;
-  public _serie;
-  public _valor;
-  public _activo;
-  public _refer;
+  public _placa: any = 0;
+  public _clase: any = 0;
+  public _nombre: any = 0;
+  public _custodio: any = 0;
+  public _dpto: any = 0;
+  public _serie: any = 0;
+  public _valor: any = 0;
+  public _activo: any = 0;
+  public _refer: any = 0;
   public _fac:any = 0;
   public _fec;
-  public _usuc;
+  public _usuc: any = 0;
   public _fmod: any = 0;
-  public _usm;
-  public _ufin;
-  public _grupo;
+  public _usm: any = 0;
+  public _ufin: any = 0;
+  public _grupo: any = 0;
+  public _marca: any = 0;
   public _color;
-  public _prov;
-  public _mp;
-  public _vut;
-  public _vre;
+  public _prov: any = 0;
+  public _mp: any = 0;
+  public _vut: any = 0;
+  public _vre: any = 0 ;
   public _ffin: any = 0;
   public _finc: any = 0 ;
   public _fcu: any = 0;
+  public _ide: any = 0;
+  public _fComp: any = 0;
   public _cgas;
   public _cdan;
   public _cdar;
   public _vnor;
   public _vrev;
-  public _img;
+  public _img: any = 0;
   public _fc: any = 0;
   // ngModel fin
 
   constructor() { }
 
   ngOnInit() {
-    
   }
 
   fac(id) {
@@ -108,7 +130,7 @@ export class ConfigurationsComponent implements OnInit {
         this.fecFin = true;
         break;
     }
-    console.log(this._ffin)
+    // console.log(this._ffin)
   }
 
   feCrea(id) {
@@ -131,9 +153,8 @@ export class ConfigurationsComponent implements OnInit {
   }
 
   feCust(id) {
-
     let a = <HTMLInputElement> document.getElementById('_fCust'); 
-    // console.log(a);   
+   // console.log(a);   
     this._fcu = a.value;
     this.dynamicSwitch(a, id, a);
     switch(this.feCusto){
@@ -144,32 +165,357 @@ export class ConfigurationsComponent implements OnInit {
         this.feCusto = true;
         break;
     }
+   // console.log(this.feCusto);
+  }
 
-    console.log(this._fcu)
-
+  feIndepre(id) {
+    let a = <HTMLInputElement> document.getElementById('_iDep'); 
+    // console.log(a);   
+    this._ide = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.feInde){
+      case '0':
+        this.feInde = false;
+        break;
+      case '1':
+        this.feInde = true;
+        break;
+    }
+    console.log(this.feInde)
+  }
+  
+  IMG(id) {
+    let a = <HTMLInputElement> document.getElementById('img'); 
+    // console.log(a);   
+    this._img = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.Imge){
+      case '0':
+        this.Imge = false;
+        break;
+      case '1':
+        this.Imge = true;
+        break;
+    }
+   // console.log(this.Imge)
   }
 
   feComp(id) {
+    let a = <HTMLInputElement> document.getElementById('fComp'); 
+    // console.log(a);   
+    this._ide = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.fCompra){
+      case '0':
+        this.fCompra = false;
+        break;
+      case '1':
+        this.fCompra = true;
+        break;
+    }
+    // console.log(this.fCompra)
+  }
 
-    let a = <HTMLInputElement> document.getElementById('_fCust'); 
-    console.log(a);   
-    // this._fcu = a.value;
-    // this.dynamicSwitch(a, id, a);
-    // switch(this.feCusto){
-    //   case '0':
-    //     this.feCusto = false;
-    //     break;
-    //   case '1':
-    //     this.feCusto = true;
-    //     break;
-    // }
+  cPlaca(id) {
 
-    // console.log(this._fcu)
+    let a = <HTMLInputElement> document.getElementById('_pla'); 
+   // console.log(a);
+    this._placa = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.placa){
+      case '0':
+        this.placa = false;
+        break;
+      case '1':
+        this.placa = true;
+        break;
+    }
+
+    // console.log(this.fCompra)
+
+  }
+  
+  Class(id) {
+
+    let a = <HTMLInputElement> document.getElementById('_class'); 
+    // console.log(a);
+    this._clase = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.Clas) {
+      case '0':
+        this.Clas = false;
+        break;
+      case '1':
+        this.Clas = true;
+        break;
+    }
+
+    // console.log(this.fCompra)
 
   }
 
+  nProduc(id) {
+
+    let a = <HTMLInputElement> document.getElementById('_nPr'); 
+    // console.log(a);
+    this._nombre = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.nProd) {
+      case '0':
+        this.nProd = false;
+        break;
+      case '1':
+        this.nProd = true;
+        break;
+    }
+
+    // console.log(this.fCompra)
+
+  }
+
+  nDeps(id) {
+    let a = <HTMLInputElement> document.getElementById('_dpto');
+    // console.log(a);
+    this._dpto = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.ndepar) {
+      case '0':
+        this.ndepar = false;
+        break;
+      case '1':
+        this.ndepar = true;
+        break;
+    }
+    // console.log(this.fCompra)
+  }
+
+  nCust(id) {
+
+    let a = <HTMLInputElement> document.getElementById('_custodio');
+    // console.log(a);
+    this._custodio = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.nCustodios) {
+      case '0':
+        this.nCustodios = false;
+        break;
+      case '1':
+        this.nCustodios = true;
+        break;
+    }
+    // console.log(this.fCompra)
+  }
+  
+  SerieFunc(id) {
+    let a = <HTMLInputElement> document.getElementById('_serie');
+    // console.log(a);
+    this._serie = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.nSerie) {
+      case '0':
+        this.nSerie = false;
+        break;
+      case '1':
+        this.nSerie = true;
+        break;
+    }
+    // console.log(this.fCompra)
+  }
+  
+  nValorFunc(id) {
+    let a = <HTMLInputElement> document.getElementById('valor');
+    // console.log(a);
+    this._valor = a.value;
+    this.dynamicSwitch(a, id, a);    
+    switch(this.nValor) {      
+      case '0':
+        this.nValor = false;
+        break;
+      case '1':
+        this.nValor = true;
+        break;
+    }
+    // console.log(this.fCompra)
+  }
+
+  nActivoFunc(id) {
+    let a = <HTMLInputElement> document.getElementById('activo');
+    // console.log(a);
+    this._activo = a.value;
+    this.dynamicSwitch(a, id, a);    
+    switch(this.nActivo) {      
+      case '0':
+        this.nActivo = false;
+        break;
+      case '1':
+        this.nActivo = true;
+        break;
+    }
+    // console.log(this.fCompra)
+  }
+
+  nRefersFunc(id) {
+    let a = <HTMLInputElement> document.getElementById('refer');
+    // console.log(a);
+    this._refer = a.value;
+    this.dynamicSwitch(a, id, a);    
+    switch(this.refers) {
+      case '0':
+        this.refers = false;
+        break;
+      case '1':
+        this.refers = true;
+        break;
+    }
+    // console.log(this.fCompra)
+  }
+
+  nUsucr(id) {
+    let a = <HTMLInputElement> document.getElementById('usuc');
+    // console.log(a);
+    this._usuc = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.nUsuc) {
+      case '0':
+        this.nUsuc = false;
+        break;
+      case '1':
+        this.nUsuc = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
+
+  nUsmr(id) {
+    let a = <HTMLInputElement> document.getElementById('usm');
+    // console.log(a);
+    this._usm = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.nUsm) {
+      case '0':
+        this.nUsm = false;
+        break;
+      case '1':
+        this.nUsm = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
+
+  nUsFin(id) {
+    let a = <HTMLInputElement> document.getElementById('ufin');
+    // console.log(a);
+    this._ufin = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.UsFin) {
+      case '0':
+        this.UsFin = false;
+        break;
+      case '1':
+        this.UsFin = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
+
+  nGroupFun(id) {
+    let a = <HTMLInputElement> document.getElementById('grupo');
+    // console.log(a);
+    this._grupo = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.nGroup) {
+      case '0':
+        this.nGroup = false;
+        break;
+      case '1':
+        this.nGroup = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
+
+  nMarca(id) {
+    let a = <HTMLInputElement> document.getElementById('marca');
+    // console.log(a);
+    this._marca = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.mArca) {
+      case '0':
+        this.mArca = false;
+        break;
+      case '1':
+        this.mArca = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
+
+  ProvFunc(id) {
+    let a = <HTMLInputElement> document.getElementById('prov');
+    // console.log(a);
+    this._prov = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.ProvVar) {
+      case '0':
+        this.ProvVar = false;
+        break;
+      case '1':
+        this.ProvVar = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
+
+  ModFunc(id) {
+    let a = <HTMLInputElement> document.getElementById('mp');
+    // console.log(a);
+    this._mp = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.ModVar) {
+      case '0':
+        this.ModVar = false;
+        break;
+      case '1':
+        this.ModVar = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
+
+  VitFunc(id) {
+    let a = <HTMLInputElement> document.getElementById('vut');
+    // console.log(a);
+    this._vut = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.VitVar) {
+      case '0':
+        this.VitVar = false;
+        break;
+      case '1':
+        this.VitVar = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
+
+  ValrFunc(id) {
+    let a = <HTMLInputElement> document.getElementById('vre');
+    // console.log(a);
+    this._vre = a.value;
+    this.dynamicSwitch(a, id, a);
+    switch(this.ValrVar) {
+      case '0':
+        this.ValrVar = false;
+        break;
+      case '1':
+        this.ValrVar = true;
+        break;
+    }
+    // console.log(this.nUsuc)
+  }
 
 
+  //configuraciones
   changeTagColor(id, cid, color) {
     let b = document.getElementById(id)
     let c = document.getElementById(cid);
@@ -217,7 +563,7 @@ export class ConfigurationsComponent implements OnInit {
    if (mes < 10)
      mes = '0' + mes; // agrega cero si el menor de 10
    this.datenow = ano + '-' + mes + '-' + dia;
- }
+  }
 
 }
 
